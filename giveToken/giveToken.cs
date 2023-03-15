091dda52765279818b9a96abf2db9bddeab3ee0a
K-Kizuku <kotani.kizuku@gmail.com>
2023-03-15 09:08:19 +0900

    init commit

---
+++ giveToken/giveToken.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace giveToken
+{
+    public class giveToken
+    {
+        private static HttpClient _client;
+        static giveToken()
+        {
+            // 初期化処理
+            _client = new HttpClient();
+        }
+        [FunctionName("giveToken")]
+        public void Run([TimerTrigger("0 30 9 * * *")]TimerInfo myTimer, ILogger log)
+        {
+            _client.GetAsync("https://app-backend-debug.azurewebsites.net/api/transaction/give");
+        }
+    }
+}
+
